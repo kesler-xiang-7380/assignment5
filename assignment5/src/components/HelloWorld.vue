e0deae2a7b0017fc6ce1358ab5f66598dafb9d21
Kesler <kesler.xiang@student.tdsb.on.ca>
2023-06-01 19:19:41 -0400

    first commit

--- assignment5/src/components/HelloWorld.vue
+++ assignment5/src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ const count = ref(12)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count -= 23">count is {{ count }}</button>
+    <button type="button" @click="count -= 20">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
