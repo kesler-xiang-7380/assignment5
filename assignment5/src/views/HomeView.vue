a465604afab429260d80ced354d08c57ae23bc38
Kesler <kesler.xiang@student.tdsb.on.ca>
2023-06-03 17:19:26 -0400

    added more content

---
+++ assignment5/src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import Header from "../components/Header.vue";
+import Hero from "../components/Hero.vue";
+import Footer from "../components/Footer.vue";
+import axios from "axios";
+import { useStore } from "../store/index.js";
+
+const store = useStore();
+const movies = (
+  await axios.get("https://api.themoviedb.org/3/movie/popular", {
+    params: {
+      api_key: import.meta.env.VITE_TMDB_API_KEY,
+      region: "US",
+      language: "en",
+      include_adult: false,
+    },
+  })
+).data;
+
+store.movies = movies.results.map((movie) => {
+  return {
+    id: movie.id,
+    poster: movie.poster_path,
+  }
+});
+</script>
+
+<template>
+  <div>
+    <Header />
+    <Hero />
+    <Footer />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ assignment5/src/views/LoginView.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {ref} from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const username = ref("");
+const password = ref("");
+
+const login = () => {
+  if (username.value === "tmdb" && password.value === "movies") {
+    router.push("/purchase")
+  } else {
+    alert("Invalid username/password!")
+  }
+  console.log(username.value, password.value);
+}
+</script>
+
+<template>
+  <form class="login-container" @submit.prevent="login()">
+    <input type="text" placeholder="username" v-model="username" />
+    <input type="password" placeholder="password" v-model="password" />
+    <input type="submit" value="Login" />
+  </form>
+</template>
+
+<style>
+.login-container {
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+}
+</style>
